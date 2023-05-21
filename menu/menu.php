43784ac611df4fadd40b95d92a83fd1a49c630c2
Linkakame <dark.link1@outlook.com>
2023-05-21 08:34:25 +0100

    test
    premiere version du menu

---
+++ menu/menu.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+    <nav>
+        <ul>
+            <li><a href="#">Cours Complets</a></li>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">A propos</a></li>
+        </ul>
+</nav>
+    </body>
+</html>
